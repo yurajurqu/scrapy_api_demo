a154694bfbddf5c0f51cd4e272e10e368832e017
obarboza <obarboza@pe.ibm.com>
2021-03-10 21:38:55 -0500

    first commit

---
+++ scrapy_api_demo/spiders/quotes.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['https://quotes.toscrape.com/api/quotes?page=1']
+
+    def parse(self, response):
+        print(response.body)
